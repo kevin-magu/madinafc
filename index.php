cd315d4a136ab54fda8ca85b0d19c4d890ed1ef1
kevin-magu <wanjaukevinmagu@gmail.com>
2024-10-28 16:07:39 +0300

    madina

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php include 'includes/navbar.html' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="stylesheet" href="styles/index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
     
