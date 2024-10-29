cf5bbb1055b6a309bc3016d3507f130d80bd69fb
kevin-magu <wanjaukevinmagu@gmail.com>
2024-10-29 09:05:44 +0300

    madina

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php include 'includes/navbar.html' ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +8,40 @@
     <title>Home</title>
 </head>
 <body>
+<?php include 'includes/navbar.html' ?>
     <section class="homepage-wrapper">
+        <div class="homepage-overlay"></div>
+        <div class="homepage-bottom">
+            <div class="club-name-wrapper">
+                <p class="club-name">
+                    <span>MADINA</span>
+                    <SPAN>FOOTBALL</SPAN>
+                    <span>CLUB</span>
+                </p>
+                <p class="club-slogan">Unity,Passion, Victory</p>
+            </div>
+            <div class="hero-upcoming-and-match-results-wrapper hero-upcoming-match-">
+                <p class="title">Upcoming Match</p>
+                <div class="card">
+                    <p class="league-name">Wajir County League</p>
 
+
+                </div>
+            </div>
+            <div class="hero-upcoming-and-match-results-wrapper hero-recent-result">
+                <p class="title">Recent Match Results</p>
+                <div class="card">
+                    
+                </div>
+            </div>
+
+            <div class="hero-buttons-wrapper">
+                <a href=""><button>Upcoming Matches</button></a>
+                <a href=""><button>Support Us</button></a>
+                
+                
+            </div>
+        </div>
     </section>
 </body>
 </html>
