a19c025dfaf0324c63f7d8867b260bc1948f9efe
kevin-magu <wanjaukevinmagu@gmail.com>
2024-11-09 10:15:33 +0300

    madina

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -3,16 +3,19 @@
 <head>
     <link rel="icon" href="../images/madina-logo.png" />
     <script src="https://kit.fontawesome.com/e4c074505f.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./styles/navbar.css">
-    <link rel="stylesheet" href="./styles/common-styles.css">
-    <link rel="stylesheet" href="./styles/footer.css">
+    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/common-styles.css">
+    <link rel="stylesheet" href="../styles/footer.css">
+    <link rel="stylesheet" href="../styles/aboutus.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
 </head>
 <body>
     <section class="aboutus-page-wrapper">
-        <?php include 'includes/navbar.html' ?>
+        <?php include '../includes/navbar.html' ?>
+            <p class="section-title">Our Mission</p>
+        <?php include '../includes/footer.html' ?>
     </section>
 </body>
 </html>
