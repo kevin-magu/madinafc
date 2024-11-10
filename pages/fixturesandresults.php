1ebccd28eb13302606f6bb3237fdc09b5dc3e3c0
kevin-magu <wanjaukevinmagu@gmail.com>
2024-11-10 12:22:36 +0300

    madina

---
+++ pages/fixturesandresults.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="../images/madina-logo.png" />
+    <script src="https://kit.fontawesome.com/e4c074505f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/common-styles.css">
+    <link rel="stylesheet" href="../styles/footer.css">
+    <link rel="stylesheet" href="../styles/fixtures.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fixtures | Results</title>
+</head>
+<body>
+    <?php include '../includes/navbar.html' ?>
+    <section class="fixtures-body-wrapper">
+        <div class="fitures-highlights">
+            <div class="section1 upcoming-match"></div>
+            <div class="section2 recent-match"></div>
+        </div>
+    </section>
+    <?php include '../includes/footer.html' ?>
+</body>
+</html>
