ebc663e8e388ddb079d1df791b6dd16051b811fd
kevin-magu <wanjaukevinmagu@gmail.com>
2024-11-19 10:12:07 +0300

    madina

---
+++ includes/connection.php
@@ -0,0 +1,13 @@
+<?php 
+$server = "localhost";
+$username = "root";
+$password = "root";
+$database = "madina_test";
+
+$connection = mysqli_connect($server, $username, $password, $database);
+if($connection){
+    echo("database connection was successful");
+}else{
+    echo("database connection failed");
+}
+?>
--- pages/fixturesandresults.php
+++ pages/fixturesandresults.php
@@ -1,3 +1,12 @@
+<?php 
+include '../includes/connection.php';
+ini_set('display_errors', 1);
+
+$query = 'SELECT * FROM fixtures';
+$query_exe = mysqli_query($connection, $query);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,15 +18,32 @@
     <link rel="stylesheet" href="../styles/fixtures.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fixtures | Results</title>
+    <title>Fixtures & Results</title>
 </head>
 <body>
     <?php include '../includes/navbar.html' ?>
     <section class="fixtures-body-wrapper">
         <div class="fitures-highlights">
-            <div class="section1 upcoming-match"></div>
-            <div class="section2 recent-match"></div>
+            <a href="" class="section1 upcoming-match">
+                <p class="upcoming-match-title">Upcoming Match</p>
+                <p class="teams"><span>Madina FC</span><span>vs</span> <span>Wajir FC</span></p>
+                <p class="location-and-time"><span>Wajir Field</span> <span>6.40pm</span> </p>
+            </a>
+            <a href="" class="section2 recent-match">
+                <p class="upcoming-match-title">Recent Match</p>
+                <p class="teams"><span>Madina FC</span> <span class="goal-number home-goal-number">4</span> : <span class="goal-number away-goal-number">1</span> <span>Wajir FC</span></p>
+                <p class="location-and-time"><span>Wajir Field</span> <span>6.40pm</span> </p>
+            </a>
         </div>
+
+        <section class="fixtures-and-results">
+            <div class="upcoming-matches">
+                <p></p>
+            </div>
+            <div class="recent-results">
+
+            </div>
+        </section>
     </section>
     <?php include '../includes/footer.html' ?>
 </body>
