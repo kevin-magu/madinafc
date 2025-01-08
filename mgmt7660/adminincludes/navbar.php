40f16fc474789d62bacb6fc049d01343ac9ab06a
kevin-magu <wanjaukevinmagu@gmail.com>
2025-01-08 21:47:38 +0300

    madina

--- mgmt7660/adminincludes/navbar.php
+++ mgmt7660/adminincludes/navbar.php
@@ -5,4 +5,12 @@
         <div class="line line3"></div>
     </div>
     <p>MADINA FC ADMIN DASHBOARD</p>
+
+    <div class="side-menu">
+        <ul>
+            <a href=""><li> <i class="fa-solid fa-user"></i> Management</li></a>
+            <a href=""><li><i class="fa-solid fa-plus"></i> Add player</li></a>
+            <a href=""><li><i class="fa-solid fa-bell"></i> Notifications</li></a>
+        </ul>
+    </div>
 </nav>
--- mgmt7660/index.php
+++ mgmt7660/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script src="https://kit.fontawesome.com/e4c074505f.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
