b15789c8e6fb6865c833d330a617f7e8e38b5f50
kevin-magu <wanjaukevinmagu@gmail.com>
2025-01-13 12:43:54 +0300

    madina

--- mgmt7660/scripts.js
+++ mgmt7660/scripts.js
@@ -2,12 +2,5 @@ var menu = document.querySelector('.menu');
 var sideMenu = document.querySelector('.side-menu');
 
 menu.addEventListener('click', function menuToglle(){
-    if( sideMenu.style.display = 'flex' ){
-        sideMenu.style.display = "none";
-        console.log(sideMenu.style.display)
-    }else if(sideMenu.style.display = 'none'){
-        sideMenu.style.display = "flex"
-        console.log(sideMenu.style.display)
-    }
     
 })
