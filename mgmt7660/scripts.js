fa6384013cb7092d1a640e7244771c6e544505d3
kevin-magu <wanjaukevinmagu@gmail.com>
2025-01-13 12:50:12 +0300

    madina

--- mgmt7660/scripts.js
+++ mgmt7660/scripts.js
@@ -2,5 +2,5 @@ var menu = document.querySelector('.menu');
 var sideMenu = document.querySelector('.side-menu');
 
 menu.addEventListener('click', function menuToglle(){
-    
+    sideMenu.classList.toggle('hidden');
 })
